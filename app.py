3b9ab9b67afdf83552a2bec71b0a81fa1616037a
Moe Elias <moeelias@gmail.com>
2018-09-28 12:02:56 -0700

    Initial commit - barebones flask app

---
+++ app.py
@@ -0,0 +1,28 @@
+"""
+app.py
+
+- contains flask app startup logic + routes
+"""
+
+from flask import (
+    Flask,
+    render_template
+)
+
+app = Flask(
+    __name__,
+    template_folder="./templates"
+)
+
+
+@app.route('/')
+def main():
+    """
+    app entry point - renders welcome/login page
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
+
